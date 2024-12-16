762ec7c572e8b709c437d706ff7b63fad5427b39
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-16 20:33:57 +0530

    Generic Button & TypeScript

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface ButtonProps {
+		children: Snippet;
+		onclick: ((e: MouseEvent) => void) | (() => void);
+		className?: string;
+	}
+	let { children, className, ...props }: ButtonProps = $props();
+</script>
+
+<button class={`btn ${className}`} {...props}>
+	{@render children()}
+</button>
--- src/lib/components/Sections/HeroSection.svelte
+++ src/lib/components/Sections/HeroSection.svelte
@@ -1,8 +1,18 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { Button } from '$components';
+
+    function onclick() {
+        goto("/#contact-form");
+    }
+</script>
+
 <section class="hero-section default-margin">
 	<h3>Hello! I'm Shubhendu</h3>
 	<div class="underscore mb-m"></div>
 	<h1>I'm a Developer crafting fast and safe libraries</h1>
 	<h1 class="dark-grey">with Rust & Python</h1>
+	<Button {onclick}>Let's talk</Button>
 </section>
 
 <style>
