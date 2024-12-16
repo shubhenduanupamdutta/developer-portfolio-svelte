204df0e767094de3af57854f16f3e0d9860b3592
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-16 20:17:07 +0530

    Hero Section

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,4 +1,12 @@
 <section class="hero-section default-margin">
-    <h3>Hello</h3>
-    <div class="underscore mb-m"></div>
+	<h3>Hello! I'm Shubhendu</h3>
+	<div class="underscore mb-m"></div>
+	<h1>I'm a Developer crafting fast and safe libraries</h1>
+	<h1 class="dark-grey">with Rust & Python</h1>
 </section>
+
+<style>
+	.hero-section {
+		padding-top: 60px;
+	}
+</style>
