913c5deb0c425665e9e11adafe3602c252f575f7
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-19 20:10:01 +0530

    Work Experience Section - CSS

--- src/lib/components/ExperienceTable.svelte
+++ src/lib/components/ExperienceTable.svelte
@@ -27,13 +27,13 @@
 	];
 </script>
 
-<section class="default-margin work-experience mt-m">
+<section class="default-margin work-experience mt-l">
 	<ul class="work-experience-list">
 		{#each workExperience as job}
 			<li class="work-item">
 				<article>
 					<h3 class="semi-bold mb-xs">{job.jobTitle}</h3>
-					<div class="comapny-and-date">
+					<div class="company-and-date">
 						<p>{job.company}</p>
 						<p class="dark-grey">
 							{job.startDate}
@@ -50,3 +50,37 @@
 	</ul>
 	<h2 class="headline">Past Work <br /> Experience</h2>
 </section>
+
+<style>
+    .work-experience {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+    }
+
+    .work-experience-list {
+       width: 50%;
+    }
+
+    .headline {
+        text-align: right;
+    }
+
+    .work-item {
+        border-bottom: 2px solid #f0eded;
+        padding-bottom: 12px;
+    }
+
+    .work-item:not(:first-of-type) {
+        padding-top: 16px;
+    }
+
+    .work-item p {
+        margin-bottom: 0;
+    }
+
+    .company-and-date {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
