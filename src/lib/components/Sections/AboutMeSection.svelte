9e943ce01ff353319d14ffd490d1a38cc05f18ea
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-19 19:29:30 +0530

    About me section - CSS

--- src/lib/components/Sections/AboutMeSection.svelte
+++ src/lib/components/Sections/AboutMeSection.svelte
@@ -35,3 +35,22 @@
 		</div>
 	</div>
 </section>
+
+<style>
+    .content-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: stretch;
+    }
+
+    .image {
+        width: 40%;
+        border-radius: 20px;
+        object-fit: cover;
+    }
+
+    .text {
+        width: 55%;
+        text-align: left;
+    }
+</style>
