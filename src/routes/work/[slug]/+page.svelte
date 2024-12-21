1db46143ce716e2956823018d0a8a9c7bf743360
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-21 13:34:18 +0530

    Project Page - HTML & CSS

--- src/routes/work/[slug]/+page.svelte
+++ src/routes/work/[slug]/+page.svelte
@@ -1,4 +1,68 @@
 <script lang="ts">
-	const { data } = $props();
+	let { data } = $props();
+
+	const { company, name, dateAccomplished, stack, projectImageUrl, content } = data.project;
 	$inspect(data);
+
+	function getTagFromStyle(style: ProcessedTextContent['style']): string {
+		if (style === 'normal') return 'p';
+		return style;
+	}
 </script>
+
+<main class="default-margin work-page">
+	<h4>{company}</h4>
+	<div class="underscore"></div>
+	<h2 class="mb-s">{name}</h2>
+	<img class="project-image" src={projectImageUrl} alt="" />
+
+	<div class="project-container mt-m">
+		<div class="meta-data">
+			<h3 class="semi-bold">Date</h3>
+			<p>{dateAccomplished.slice(0, 7)}</p>
+			<h3 class="semi-bold mt-m">Tech Stack</h3>
+			<ul>
+				{#each stack as skill}
+					<li>{skill}</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="project-text">
+			{#each content as block}
+				{#if block.type === 'text'}
+					<svelte:element this={getTagFromStyle(block.style)}>{block.textToRender}</svelte:element>
+				{/if}
+
+				{#if block.type === 'image'}
+					<img class="content-image" src={block.url} alt="" />
+				{/if}
+			{/each}
+		</div>
+	</div>
+</main>
+
+<style>
+    .work-page {
+        padding-top: 80px;
+        padding-bottom: 140px;
+    }
+
+    .project-image {
+        width: 100%;
+        max-height: 450px;
+        object-fit: fill;
+    }
+
+    .content-image {
+        width: 80%;
+        margin: 0 auto;
+    }
+
+    .project-container {
+        display: flex;
+    }
+
+    .meta-data {
+        min-width: 200px;
+    }
+</style>
