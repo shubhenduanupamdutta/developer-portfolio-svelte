548e0fd9eaaf529e3a50b22da16a4915c9e19b46
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-21 14:42:22 +0530

    Contact Section - CSS

--- src/lib/components/Sections/ContactSection.svelte
+++ src/lib/components/Sections/ContactSection.svelte
@@ -17,7 +17,7 @@
 			<Button onclick={onSubmit}>Submit</Button>
 		</form>
 		<div class="form-text">
-			<h3 class="bold mb-s">Talk to me about your project.</h3>
+			<h3 class="bold mb-m">Talk to me about your project.</h3>
 			<p>
 				I'm always looking for new opportunities to work on exciting projects. If you have a project
 				in mind, let's talk about it. I am currently focusing on web development, specifically using
@@ -26,3 +26,96 @@
 		</div>
 	</div>
 </section>
+
+<style>
+	section {
+		padding-bottom: 140px;
+	}
+
+	.form-container {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.form-text {
+		width: 40%;
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		width: 50%;
+	}
+
+	form * {
+		font-size: 20px;
+		font-family: 'Inter Tight', sans-serif;
+		font-weight: 500;
+		color: black;
+	}
+
+	textarea,
+	input {
+		width: 100%;
+		background-color: rgba(0, 0, 0, 0.035);
+		border-radius: 8px;
+		padding: 4px 12px;
+		outline: none;
+		border: none;
+	}
+
+	input {
+		height: 48px;
+	}
+
+	textarea {
+		height: 120px;
+		margin-bottom: 40px;
+	}
+
+	p {
+		font-size: 18px;
+		text-align: justify;
+	}
+
+	textarea::placeholder,
+	input::placeholder {
+		font-size: 20px;
+		font-family: 'Inter Tight', sans-serif;
+		font-weight: 400;
+	}
+
+	.input-error {
+		background-color: rgba(223, 87, 87, 0.667);
+	}
+
+	.input-error::placeholder {
+		color: white;
+	}
+
+	.spinner {
+		border: 4px solid rgba(0, 0, 0, 0.1);
+		border-left-color: black;
+		border-radius: 50%;
+		width: 16px;
+		height: 16px;
+		display: inline-block;
+
+		margin-right: 8px;
+		animation: spin 1s linear infinite;
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
+    .spinner-container {
+        display: flex;
+    }
+</style>
