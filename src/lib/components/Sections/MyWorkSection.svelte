99d74df6169dce92c56270cb7316903ec5792d1a
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-21 13:03:20 +0530

    My Work Section - CSS

--- src/lib/components/Sections/MyWorkSection.svelte
+++ src/lib/components/Sections/MyWorkSection.svelte
@@ -18,7 +18,7 @@
 			</a>
 			<div class="project-info mt-s">
 				<div class="title-and-company">
-					<h3 class="semi-bold">{mainProject.name}</h3>
+					<h4 class="semi-bold">{mainProject.name}</h4>
 					<p class="company dark-grey">{mainProject.company}</p>
 				</div>
 				<a href={`/work/${mainProject.slug}`} class="button-to-article">&rarr;</a>
@@ -32,7 +32,7 @@
 					</a>
 					<div class="project-info mt-s">
 						<div class="title-and-company">
-							<h3 class="semi-bold">{project.name}</h3>
+							<h4 class="semi-bold">{project.name}</h4>
 							<p class="company dark-grey">{project.company}</p>
 						</div>
 						<a href={`/work/${project.slug}`} class="button-to-article">&rarr;</a>
@@ -42,3 +42,57 @@
 		</div>
 	</div>
 </section>
+
+<style>
+    img {
+        width: 100%;
+        border-radius: 20px;
+        object-fit: fill;
+        cursor: pointer;
+    }
+
+    .more-projects-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    .more-projects-container article {
+        width: 45%;
+    }
+
+    .more-projects-container img {
+        height: 200px;
+    }
+
+    .main-project img {
+        max-height: 450px;
+    }
+
+    article {
+        margin: 10px;
+        padding: 10px;
+        border: 2px solid #555;
+        box-shadow: 0 0 10px #888;
+        border-radius: 20px;
+    }
+
+    .project-info {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .button-to-article {
+        display: block;
+        font-size: 32px;
+        border: 1px solid black;
+        border-radius: 32px;
+        padding: 0 20px 5px;
+    }
+
+    .company {
+        font-size: 12px;
+        margin-bottom: 0;
+    }
+</style>
